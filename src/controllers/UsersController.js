d6f761fe7b06d1d2e1bfcc62214c830d172780fc
douglas <38635101+dougdbp@users.noreply.github.com>
2023-04-26 21:37:17 -0300

    created services at SRC

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,8 +1,9 @@
 const { hash, compare } = require("bcryptjs");
 const AppError = require("../utils/AppError");
 
-const UserRepository = require("../repositories/UserRepository")
+const UserRepository = require("../repositories/UserRepository");
 const sqliteConnection = require("../database/sqlite");
+const UserCreateService = require("../services/UserCreateService");
 
 class UsersController {
     /*
@@ -17,16 +18,9 @@ class UsersController {
         const { name, email, password } = request.body;
 
         const userRepository = new UserRepository();
-    
-        const checkUserExists = await userRepository.findByEmail(email);
-
-        if(checkUserExists){
-            throw new AppError("Este e-mail já está em uso.");
-        }
-
-        const hashedPassword = await hash(password, 8);
-
-        await userRepository.create({ name, email, password: hashedPassword });
+        const userCreateService = new UserCreateService(userRepository);
+        
+        await userCreateService.execute({ name, email, password });
 
         return response.status(201).json();
     }
